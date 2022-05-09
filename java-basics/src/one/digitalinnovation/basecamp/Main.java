58cc66faf35fa22ab3d43f451ace4cc7b511b7d4
scatteredsky <67299222+scatteredsky@users.noreply.github.com>
2022-05-08 22:59:04 -0500

    updating files/folders

---
+++ java-basics/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ * * exemplo de Classe para o exercício da Aula 1 de Java (variáveis, Tipos de dado, e Operadores aritméticos.
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        int I;
+        int $aq;
+        int _1a;
+
+        i = 5;
+        I = 10;
+        $aq = 7;
+        _1a =20;
+
+    }
+}
---
+++ java-ide-course/src/com/dio/PrimeiroProgramaIde.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+public class PrimeiroProgramaIde {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá, Mundo!");
+
+	}
+
+}
